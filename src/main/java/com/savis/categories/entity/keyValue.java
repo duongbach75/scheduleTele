7b4a29c94452121363020a3adcd122b33e27bc41
Dương Hoàng Bách <duongbach48@gmail.com>
2021-03-09 17:15:07 +0700

    sdf

---
+++ src/main/java/com/savis/categories/entity/keyValue.java
@@ -0,0 +1,16 @@
+package com.savis.categories.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class keyValue {
+	private String key;
+	private String value;
+	private String type;
+}
--- src/main/java/com/savis/categories/telegram/MyAmazingBot.java
+++ src/main/java/com/savis/categories/telegram/MyAmazingBot.java
@@ -1,45 +1,66 @@
 package com.savis.categories.telegram;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.api.objects.PhotoSize;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardRemove;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-
-import com.savis.categories.common.ExcelPoiUtil;
-import com.savis.categories.common.UtilsDate;
-import com.savis.categories.entity.schedule;
-import com.savis.categories.service.serviceSchedule;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
+import org.springframework.util.ResourceUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.api.objects.PhotoSize;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import com.savis.categories.common.ExcelPoiUtil;
+import com.savis.categories.common.UtilsDate;
+import com.savis.categories.entity.keyValue;
+import com.savis.categories.entity.schedule;
+import java.nio.file.Path;
 
 public class MyAmazingBot extends TelegramLongPollingBot {
 	@Autowired
-	private serviceSchedule serviceSchedule;
-
+	ResourceLoader resourceLoader;
+	List<String> list= new ArrayList<String>();
+	List<String> blacklist= new ArrayList<String>();
+	Random randomGenerator= new Random();
 	@Override
 	public void onUpdateReceived(Update update) {
-
+		list.add("Thứ 2 được không?");
+		list.add("Thứ 3 được không?");
+		list.add("Thứ 4 được không?");
+		list.add("Thứ 5 được không?");
+		list.add("Thứ 6 được không?");
+		list.add("Thứ 7 được không?");
+		list.add("Chủ nhật được không?");
+		
+		blacklist.add("dmm");
+		blacklist.add("địt");
+		blacklist.add("cặc");
+		blacklist.add("lồn");
+		blacklist.add("đéo");
+		blacklist.add("dcm");
+		blacklist.add("deo");
+		blacklist.add("cac");
+		
 		// We check if the update has a message and the message has text
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			// Set variables
@@ -54,89 +75,69 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 					e.printStackTrace();
 				}
 			} else if (message_text.equals("/schedule")) {
-				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(schedule());
+				SendMessage message=null;
+				try {
+					message = new SendMessage() // Create a message object object
+							.setChatId(chat_id).setText(schedule());
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			} else if (message_text.equals("/Bia")) {
-                SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
-                        .setText("Hôm nào?");
-                // Create ReplyKeyboardMarkup object
-                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-                // Create the keyboard (list of keyboard rows)
-                List<KeyboardRow> keyboard = new ArrayList<>();
-                // Create a keyboard row
-                KeyboardRow row = new KeyboardRow();
-                // Set each button, you can also use KeyboardButton objects if you need something else than text
-                row.add("thu 2");
-                row.add("thu 3");
-                row.add("thu 4");
-                // Add the first row to the keyboard
-                keyboard.add(row);
-                // Create another keyboard row
-                row = new KeyboardRow();
-                // Set each button for the second line
-                row.add("thu 5");
-                row.add("thu 6");
-                row.add("chu nhat");
-                // Add the second row to the keyboard
-                keyboard.add(row);
-                // Set the keyboard to the markup
-                keyboardMarkup.setKeyboard(keyboard);
-                // Add it to the message
-                message.setReplyMarkup(keyboardMarkup);
-                try {
-                    sendMessage(message); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }else if (message_text.equals("thu 2")) {
-            	SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+			} else if (message_text.indexOf("Niệm")!=-1) {
+				SendMessage message = new SendMessage() // Create a message object object
+						.setChatId(chat_id).setText("Trưởng phòng Cloud Viettel");
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			}else if (message_text.equals("thu 3")) {
+			} else if (message_text.indexOf("Trọng")!=-1) {
 				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+						.setChatId(chat_id).setText("Tổng giám đốc tập đoàn Shapro");
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			}else if (message_text.equals("thu 4")) {
+			} else if (message_text.equals("/bia")) {
+
+				
 				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+						.setChatId(chat_id).setText(list.get(randomGenerator.nextInt(list.size())));
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			}else if (message_text.equals("thu 5")) {
+			} else if (message_text.equals("/khong")) {
 				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+						.setChatId(chat_id).setText("Thế mày rảnh hôm nào? "+list.get(randomGenerator.nextInt(list.size())));
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			}else if (message_text.equals("thu 6")) {
+			} else if (message_text.equals("/ok")) {
 				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+						.setChatId(chat_id).setText("Chốt nhá");
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			}else if (message_text.equals("chu nhat")) {
+			} else if (checklist(message_text)==false) {
+				List<String> blacklist= new ArrayList<String>();
+				blacklist.add("im mồm đmm, cấm nói bậy");
+				blacklist.add("Địt mẹ thằng láo cá chó này");
+				blacklist.add("Mày đừng có mà ương");
+				blacklist.add("á à đmm thích nói bậy à");
 				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText(message_text);
+						.setChatId(chat_id).setText(blacklist.get(randomGenerator.nextInt(blacklist.size())));
 				try {
 					sendMessage(message); // Sending our message object to user
 				} catch (TelegramApiException e) {
@@ -151,15 +152,7 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 				} catch (TelegramApiException e) {
 					e.printStackTrace();
 				}
-			} else {
-				SendMessage message = new SendMessage() // Create a message object object
-						.setChatId(chat_id).setText("Unknown command");
-				try {
-					sendMessage(message); // Sending our message object to user
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-			}
+			} 
 		} else if (update.hasMessage() && update.getMessage().hasPhoto()) {
 			// Message contains photo
 			// Set variables
@@ -194,18 +187,27 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 		// TODO Auto-generated method stub
 		return "1608177542:AAFifgOQotlUFQQhTGyxHM8Me8tUKUIIv44";
 	}
-	public String schedule() {
-		 try {
-			List<schedule> listBooks = readExcel("G:\\ThoiKhoaBieuSinhVien.xls");
+public boolean checklist(String message_text) {
+	for (String string : blacklist) {
+		if (message_text.indexOf(string)!=-1)
+			return false;
+	}
+	return true;
+	
+}
+	public String schedule() throws URISyntaxException {
+		try {
+			List<schedule> listBooks = readExcel("/opt/ThoiKhoaBieuSinhVien.xls");
 			for (schedule schedule : listBooks) {
-				Date today= new Date();
-				Date from =UtilsDate.str2date(schedule.getDay().split("-")[0], "dd/MM/yyyy");
-				Date to =UtilsDate.str2date(schedule.getDay().split("-")[1], "dd/MM/yyyy");
+				Date today = new Date();
+				Date from = UtilsDate.str2date(schedule.getDay().split("-")[0], "dd/MM/yyyy");
+				Date to = UtilsDate.str2date(schedule.getDay().split("-")[1], "dd/MM/yyyy");
 				Calendar c = Calendar.getInstance();
 				c.setTime(today);
 				int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-				if(isWithinRange(today, from, to)==true && String.valueOf(dayOfWeek).equals(schedule.getNumber())) {
-					return "Ca học: "+schedule.getTime() +" Môn học: "+schedule.getName()+" Phòng học "+schedule.getRoom();
+				if (isWithinRange(today, from, to) == true && String.valueOf(dayOfWeek).equals(schedule.getNumber())) {
+					return "Ca học: " + schedule.getTime() + " Môn học: " + schedule.getName() + " Phòng học "
+							+ schedule.getRoom()+". Kính mong được gặp các sếp trên giảng đường";
 				}
 			}
 		} catch (IOException e) {
@@ -214,18 +216,23 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 			return "Lỗi rồi";
 		}
 		return "Không có ca học nào";
-		
+
 	}
-	
-    public static final int COLUMN_INDEX_ID = 3;
-    public static final int COLUMN_INDEX_TITLE = 8;
-    public static final int COLUMN_INDEX_PRICE = 10;
-    public static final int COLUMN_INDEX_QUANTITY = 9;
-    public static final int number = 0;
-    
-	public static List<schedule> readExcel(String excelFilePath) throws IOException {
-		
-        List<schedule> listBooks = new ArrayList<>();
+
+	public static final int COLUMN_INDEX_ID = 4;
+	public static final int COLUMN_INDEX_TITLE = 8;
+	public static final int COLUMN_INDEX_PRICE = 10;
+	public static final int COLUMN_INDEX_QUANTITY = 9;
+	public static final int number = 0;
+	private static String getProjectPath() throws URISyntaxException{
+	    URL url = MyAmazingBot.class.getResource("/");
+	    Path path = (Path) Paths.get(url.toURI());
+	    Path subPath = path.subpath(0, path.getNameCount() -2);
+	    return "/" + subPath.toString();
+	}
+	public static List<schedule> readExcel(String excelFilePath) throws IOException, URISyntaxException {
+
+		 List<schedule> listBooks = new ArrayList<>();
  
         // Get file
         InputStream inputStream = new FileInputStream(new File(excelFilePath));
@@ -235,63 +242,121 @@ public class MyAmazingBot extends TelegramLongPollingBot {
  
         // Get sheet
         Sheet sheet = workbook.getSheetAt(0);
- 
-        // Get all rows
-        Iterator<Row> iterator = sheet.iterator();
-        while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            if (nextRow.getRowNum() == 0) {
-                // Ignore header
-                continue;
-            }
- 
-            // Get all cells
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
- 
-            // Read cells and set value for book object
-            schedule canhanImportDTO = new schedule();
-            while (cellIterator.hasNext()) {
-                //Read cell
-                Cell cell = cellIterator.next();
-                Object cellValue = ExcelPoiUtil.getCellValue(cell);
-                if (cellValue == null || cellValue.toString().isEmpty()) {
-                    continue;
-                }
-                // Set value for book object
-                int columnIndex = cell.getColumnIndex();
-                switch (columnIndex) {
-                case COLUMN_INDEX_ID:
-                	canhanImportDTO.setName(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                case COLUMN_INDEX_TITLE:
-                	canhanImportDTO.setTime(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                case COLUMN_INDEX_QUANTITY:
-                	canhanImportDTO.setRoom(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                case COLUMN_INDEX_PRICE:
-                	canhanImportDTO.setDay(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                case number:
-                	canhanImportDTO.setNumber(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                case 1:
-                	canhanImportDTO.setCode(ExcelPoiUtil.getCellValue(cell));
-                    break;
-                }
- 
-            }
-            if(canhanImportDTO.getNumber()!=null&& StringUtils.isNumeric(canhanImportDTO.getNumber()))
-            listBooks.add(canhanImportDTO);
-        }
- 
-        workbook.close();
-        inputStream.close();
- 
-        return listBooks;
-    }
-	
-	public boolean isWithinRange(Date testDate,Date startDate,Date endDate) {
-		   return !(testDate.before(startDate) || testDate.after(endDate));
+
+		// Get all rows
+		Iterator<Row> iterator = sheet.iterator();
+		while (iterator.hasNext()) {
+			Row nextRow = iterator.next();
+			if (nextRow.getRowNum() == 0) {
+				// Ignore header
+				continue;
+			}
+
+			// Get all cells
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+			// Read cells and set value for book object
+			schedule canhanImportDTO = new schedule();
+			while (cellIterator.hasNext()) {
+				// Read cell
+				Cell cell = cellIterator.next();
+				Object cellValue = ExcelPoiUtil.getCellValue(cell);
+				if (cellValue == null || cellValue.toString().isEmpty()) {
+					continue;
+				}
+				// Set value for book object
+				int columnIndex = cell.getColumnIndex();
+				switch (columnIndex) {
+				case COLUMN_INDEX_ID:
+					canhanImportDTO.setName(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case COLUMN_INDEX_TITLE:
+					canhanImportDTO.setTime(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case COLUMN_INDEX_QUANTITY:
+					canhanImportDTO.setRoom(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case COLUMN_INDEX_PRICE:
+					canhanImportDTO.setDay(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case number:
+					canhanImportDTO.setNumber(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case 1:
+					canhanImportDTO.setCode(ExcelPoiUtil.getCellValue(cell));
+					break;
+				}
+
+			}
+			if (canhanImportDTO.getNumber() != null && StringUtils.isNumeric(canhanImportDTO.getNumber()))
+				listBooks.add(canhanImportDTO);
 		}
+
+		workbook.close();
+		inputStream.close();
+
+		return listBooks;
+	}
+
+	public boolean isWithinRange(Date testDate, Date startDate, Date endDate) {
+		return !(testDate.before(startDate) || testDate.after(endDate));
+	}
+	
+	public static List<keyValue> readExcelKeyword(String excelFilePath) throws IOException {
+
+		List<keyValue> listBooks = new ArrayList<>();
+
+		// Get file
+		InputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+		// Get workbook
+		Workbook workbook = ExcelPoiUtil.getWorkbook(inputStream, excelFilePath);
+
+		// Get sheet
+		Sheet sheet = workbook.getSheetAt(0);
+
+		// Get all rows
+		Iterator<Row> iterator = sheet.iterator();
+		while (iterator.hasNext()) {
+			Row nextRow = iterator.next();
+			if (nextRow.getRowNum() == 0) {
+				// Ignore header
+				continue;
+			}
+
+			// Get all cells
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+			// Read cells and set value for book object
+			keyValue canhanImportDTO = new keyValue();
+			while (cellIterator.hasNext()) {
+				// Read cell
+				Cell cell = cellIterator.next();
+				Object cellValue = ExcelPoiUtil.getCellValue(cell);
+				if (cellValue == null || cellValue.toString().isEmpty()) {
+					continue;
+				}
+				// Set value for book object
+				int columnIndex = cell.getColumnIndex();
+				switch (columnIndex) {
+				case 0:
+					canhanImportDTO.setKey(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case 1:
+					canhanImportDTO.setValue(ExcelPoiUtil.getCellValue(cell));
+					break;
+				case 2:
+					canhanImportDTO.setType(ExcelPoiUtil.getCellValue(cell));
+				
+				}
+
+			}
+			
+		}
+
+		workbook.close();
+		inputStream.close();
+
+		return listBooks;
+	}
 }
